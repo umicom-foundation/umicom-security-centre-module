846247150026b97731e2b4de114ecc06ac413f06
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-08-30 22:43:28 +0100

    feat(security-centre): enhance productisation contribution

--- include/umicom/security_centre/productisation_contribution.h
+++ include/umicom/security_centre/productisation_contribution.h
@@ -16,7 +16,7 @@
 #ifndef UMICOM_SECURITY_CENTRE_PRODUCTISATION_CONTRIBUTION_H
 #define UMICOM_SECURITY_CENTRE_PRODUCTISATION_CONTRIBUTION_H
 
-#include "umicom/application/productisation/adoption.h"
+#include "umicom/application/productisation/session.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +26,8 @@ const UmiProductApplicationAdoption *
 umi_security_centre_productisation_contribution(void);
 UmiStatus umi_security_centre_productisation_snapshot(
     UmiProductApplicationAdoptionSnapshot *out_snapshot);
+UmiStatus umi_security_centre_product_session_init(
+    UmiProductApplicationSession *out_session);
 
 #ifdef __cplusplus
 }
